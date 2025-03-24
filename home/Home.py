3ef2ff73e0c97628f05f888ce827dc4c87470487
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-24 13:17:27 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -151,6 +151,16 @@ else:
 
                     authors_df = merged_df.explode('authorships').reset_index(drop=True)
                     authors_df = pd.json_normalize(authors_df['authorships']).reset_index(drop=True)
+                    authors_table = authors_df[[
+                        'author.display_name',
+                        'author.orcid',
+                        'author_position',
+                        'is_corresponding',
+                        'raw_author_name'
+                    ]].drop_duplicates().reset_index(drop=True)
+
+                    st.subheader("Authors")
+                    st.dataframe(authors_table)
                     institutions_df = authors_df.explode('institutions')
                     institutions_df = pd.concat([
                         institutions_df.drop(columns=['institutions']),
