8fefa980b00df43e98d5b99644c8b89fd10e0797
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-24 13:34:59 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -141,6 +141,7 @@ else:
 
                     oa_status_summary = merged_df['open_access.oa_status'].value_counts(dropna=False).reset_index()
                     oa_status_summary.columns = ['OA_status', '# Outputs']
+                    merged_df['open_access.is_oa'] = merged_df['open_access.is_oa'].map({True: 'Open Access', False: 'Closed Access'})
                     oa_summary = merged_df['open_access.is_oa'].value_counts(dropna=False).reset_index()
                     oa_summary.columns = ['Is OA?', '# Outputs']
                     st.subheader("Open Access Status Summary", anchor=False)
