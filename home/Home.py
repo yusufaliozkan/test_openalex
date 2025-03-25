da4d8ae485c2b0ad0e5a5884c043ecd707252edc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-25 12:29:51 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -207,6 +207,7 @@ else:
                     st.subheader("Country Affiliations")
                     st.dataframe(country_freq, hide_index=True,  use_container_width=False)
 
+                    merged_df = merged_df.loc[:, ~merged_df.columns.str.startswith('abstract_inverted_index.')]
                     merged_df
 
                     end_time = time.time()
