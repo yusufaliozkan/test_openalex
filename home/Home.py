a719ecf23ca6548dfb4f7884899f138adc7add63
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-25 13:29:57 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -173,7 +173,10 @@ else:
                                 default=[] 
                                 # default=available_oa_statuses  # All selected by default
                             )
-                            filtered_df = merged_df[merged_df['open_access.oa_status'].isin(selected_statuses)]
+                            if selected_statuses:
+                                filtered_df = merged_df[merged_df['open_access.oa_status'].isin(selected_statuses)]
+                            else:
+                                filtered_df = merged_df
                             filtered_df= filtered_df.reset_index(drop=True)
                             filtered_df.index +=1
                             filtered_df = filtered_df[['doi','primary_location.source.display_name', 'publication_year', 'open_access.is_oa','open_access.oa_status', 'open_access.oa_url', 'primary_location.license_id']]
