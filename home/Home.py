760ab0db5a3e7aa2b7a6f2f2d4203f42166e77e3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-25 13:59:06 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -152,16 +152,19 @@ else:
                         merged_df['open_access.is_oa'] = merged_df['open_access.is_oa'].map({True: 'Open Access', False: 'Closed Access'})
                         oa_summary = merged_df['open_access.is_oa'].value_counts(dropna=False).reset_index()
                         oa_summary.columns = ['Is OA?', '# Outputs']
-                        if len(oa_summary) >= 1:
-                            items = [
-                                f"**{row['# Outputs']}** *{row['Is OA?']}*"
-                                for _, row in oa_summary.iterrows()
-                            ]
-                            st.write(f"{' and '.join(items)} papers found")
-                        elif len(oa_summary) == 1:
-                            st.write(f'''
-                                **{oa_summary.iloc[0]['# Outputs']}** *{oa_summary.iloc[0]['Is OA?']}* papers found.
-                            ''')
+                        st.write(f'''
+                            **{oa_summary.iloc[0]['# Outputs']}** *{oa_summary.iloc[0]['Is OA?']}* papers found.
+                        ''')
+                        # if len(oa_summary) >= 1:
+                        #     items = [
+                        #         f"**{row['# Outputs']}** *{row['Is OA?']}*"
+                        #         for _, row in oa_summary.iterrows()
+                        #     ]
+                        #     st.write(f"{' and '.join(items)} papers found")
+                        # elif len(oa_summary) == 1:
+                        #     st.write(f'''
+                        #         **{oa_summary.iloc[0]['# Outputs']}** *{oa_summary.iloc[0]['Is OA?']}* papers found.
+                        #     ''')
                         available_oa_statuses = oa_status_summary['OA status'].dropna().unique().tolist()
                         selected_statuses = st.multiselect(
                             'Filter by OA Status',
