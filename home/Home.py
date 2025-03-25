ace5200b581614e8eb7b595304d079108180c5b0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-25 13:02:59 +0000

    sd

--- home/Home.py
+++ home/Home.py
@@ -141,12 +141,12 @@ else:
                         num_results = merged_df['id'].notnull().sum()
                         st.success(f"{num_results} result(s) found.")
 
+                    # OA Summary
                     oa_status_summary = merged_df['open_access.oa_status'].value_counts(dropna=False).reset_index()
                     oa_status_summary.columns = ['OA status', '# Outputs']
                     merged_df['open_access.is_oa'] = merged_df['open_access.is_oa'].map({True: 'Open Access', False: 'Closed Access'})
                     oa_summary = merged_df['open_access.is_oa'].value_counts(dropna=False).reset_index()
                     oa_summary.columns = ['Is OA?', '# Outputs']
-                    st.subheader("Open Access Status Summary", anchor=False)
                     if len(oa_summary) >= 2:
                         items = [
                             f"**{row['# Outputs']}** *{row['Is OA?']}*"
@@ -157,8 +157,19 @@ else:
                         st.write(f'''
                             **{oa_summary.iloc[0]['# Outputs']}** *{oa_summary.iloc[0]['Is OA?']}* papers found.
                         ''')
-                    st.dataframe(oa_status_summary, hide_index =True,  use_container_width=False)
-
+                    col1, col2 = st.columns(2)
+                    st.subheader("Open Access Status Summary", anchor=False)
+                    with col1:
+                        st.dataframe(oa_status_summary, hide_index =True,  use_container_width=False)
+                    with col2:
+                        available_oa_statuses = oa_status_summary['OA status'].dropna().unique().tolist()
+                        selected_statuses = st.multiselect(
+                            'Filter by OA Status',
+                            options=available_oa_statuses,
+                            default=available_oa_statuses  # All selected by default
+                        )
+                        filtered_df = merged_df[merged_df['open_access.oa_status'].isin(selected_statuses)]
+                        filtered_df
                     # JOURNALS
                     top_journals = merged_df['primary_location.source.display_name'].value_counts(dropna=False).reset_index()
                     top_journals.columns = ['Journal name', '# Outputs']
