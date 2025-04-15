3443c42ce15edb2dc2f8a2f20e3ef478465e6c63
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-15 17:02:17 +0100

    sd

--- home/Home.py
+++ home/Home.py
@@ -167,8 +167,8 @@ else:
                                 remove = st.button('Remove selected duplicate(s)')
                                 if remove:
                                     merged_df = merged_df[~merged_df['id'].isin(selected_ids)]
-                       duplicate_function(duplicates_df, merged_df)
-                       @st.fragment 
+                    duplicate_function(duplicates_df, merged_df)
+                    @st.fragment 
 
                     merged_df = merged_df.loc[:, ~merged_df.columns.str.startswith('abstract_inverted_index.')]
                     all_results_df = merged_df.copy()
