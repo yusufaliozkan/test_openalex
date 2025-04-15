53853e38ccc295fc99b01525d630744645ecf210
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-15 15:39:45 +0100

    sd

--- home/Home.py
+++ home/Home.py
@@ -158,7 +158,7 @@ else:
                     merged_df['open_access.is_oa'] = merged_df['open_access.is_oa'].map({True: 'Open Access', False: 'Closed Access'})
                     oa_summary = merged_df['open_access.is_oa'].value_counts(dropna=False).reset_index()
                     oa_summary.columns = ['Is OA?', '# Outputs']
-
+                    duplicates_df
 
                     # OA Summary
                     @st.fragment
