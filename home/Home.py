65f4d78d3c5c7d6f1e9da6e49930aa42cd6c9a5f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-24 13:55:49 +0100

    ds

--- home/Home.py
+++ home/Home.py
@@ -400,37 +400,17 @@ else:
                                     sdg_df = filtered_raw_df.explode('sustainable_development_goals').reset_index(drop=True)
                                 else:
                                     sdg_df = merged_df.explode('sustainable_development_goals').reset_index(drop=True)
-                                sdg_df = pd.json_normalize(sdg_df['sustainable_development_goals']).reset_index(drop=True)
-                                sdg_table = sdg_df[[
-                                    'display_name'
+
+                                sdg_df
+                                
+                                authors_df = pd.json_normalize(authors_df['authorships']).reset_index(drop=True)
+                                authors_table = authors_df[[
+                                    'author.display_name',
+                                    'author.orcid',
+                                    'author_position',
+                                    'is_corresponding',
+                                    'raw_author_name'
                                 ]].drop_duplicates().reset_index(drop=True)
-                                sdg_table
-
-                                sdg_df = sdg_df.explode('institutions').reset_index(drop=True)
-                                sdg_details = pd.json_normalize(sdg_df['institutions']).reset_index(drop=True)
-                                sdg_df = pd.concat([
-                                    sdg_df.drop(columns=['institutions']).reset_index(drop=True),
-                                    sdg_details
-                                ], axis=1)
-
-                                expected_cols = ['author.display_name', 'display_name', 'country_code', 'type']
-                                for col in expected_cols:
-                                    if col not in institutions_df.columns:
-                                        institutions_df[col] = "No info"
-                                existing_cols = [col for col in expected_cols if col in institutions_df.columns]
-                                institutions_table = institutions_df[existing_cols].drop_duplicates().reset_index(drop=True)
-
-
-                                institutions_table.columns = ['author', 'institution', 'country_code', 'type']
-
-                                # st.subheader("Author Institutions")
-                                # st.dataframe(institutions_table,  use_container_width=False)
-
-                                # Institution frequency table
-                                institution_freq = institutions_table['institution'].value_counts(dropna=True).reset_index()
-                                institution_freq.columns = ['Institution', '# Count']
-                                st.subheader("Institutional Affiliations", anchor=False)
-                                st.dataframe(institution_freq, hide_index=True,  use_container_width=False)
 
 
                         st.subheader('Metrics', anchor=False)
