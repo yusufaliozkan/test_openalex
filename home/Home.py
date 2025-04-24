9422e6653a63f8589738f01ac5ea745c2f33f7de
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-24 14:57:59 +0100

    sd

--- home/Home.py
+++ home/Home.py
@@ -405,20 +405,14 @@ else:
                                 if table_view:
                                     st.dataframe(country_freq, hide_index=True, use_container_width=False)
                                 else:
-                                    fig = px.choropleth(
-                                        country_freq,
-                                        locations="Country",
-                                        locationmode="country names",
-                                        color="# Count",
-                                        color_continuous_scale="Viridis",  # More vibrant than 'Blues'
-                                        title="Country Affiliations Map"
-                                    )
-                                    fig.update_layout(
-                                        geo=dict(showframe=False, showcoastlines=False, projection_type='equirectangular'),
-                                        margin=dict(l=0, r=0, t=30, b=0),
-                                        coloraxis_colorbar=dict(title="# Affiliations")
-                                    )
-                                    col2.plotly_chart(fig, use_container_width=True)
+                                    fig = px.choropleth(country_freq,
+                                                        locations="Country",
+                                                        locationmode="country names",
+                                                        color="# Count",
+                                                        color_continuous_scale="Blues",
+                                                        title="Country Affiliations Map")
+
+                                    st.plotly_chart(fig, use_container_width=True)
 
                         st.subheader("Topics and SDGs", anchor=False)
                         with st.expander('Results', expanded= True):
